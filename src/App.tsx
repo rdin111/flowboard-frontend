0ece16631f878e22219aaf56ece83502ad915c93
rdin111 <168043983+rdin111@users.noreply.github.com>
2025-10-20 02:19:54 -0400

    analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css'
-
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
             <main className="flex-grow">
                 <Suspense fallback={<div className="p-4 text-center">Loading...</div>}>
                     <Outlet />
+                    <Analytics />
                 </Suspense>
             </main>
             <Footer />
